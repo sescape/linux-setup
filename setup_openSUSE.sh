56c9875601b23ed6274e8f2754e24f801789698e
sadsyed <sadsyed@localhost.localdomain>
2022-01-06 16:29:07 -0500

    initial commit with git and openssh

---
+++ setup_openSUSE.sh
@@ -0,0 +1,16 @@
+
+## install git
+sudo zypper install --no-confirm git-core
+
+## install openssh
+sudo zypper refresh
+sudo zypper install --no-confirm openssh
+# start sshd service via systemcd
+sudo systemctl start sshd
+# check if sshd is successfully started
+sudo systemctl status sshd
+# enable sshd during system startup
+sudo systemctl enable sshd
+# enable firewall rule for ssh
+sudo firewall-cmd --permanent --add-service=ssh
+sudo firewall-cmd reload
