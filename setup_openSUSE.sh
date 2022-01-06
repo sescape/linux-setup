0c5c171fbc2c8014db6b8581863da1c1c897808b
sadsyed <sadsyed@localhost.localdomain>
2022-01-06 17:26:40 -0500

    fixed execution errors

--- setup_openSUSE.sh
+++ setup_openSUSE.sh
@@ -9,9 +9,9 @@ sudo zypper install --no-confirm openssh
 # start sshd service via systemcd
 sudo systemctl start sshd
 # check if sshd is successfully started
-sudo systemctl status sshd
+# sudo systemctl status sshd
 # enable sshd during system startup
 sudo systemctl enable sshd
 # enable firewall rule for ssh
 sudo firewall-cmd --permanent --add-service=ssh
-sudo firewall-cmd reload
+sudo firewall-cmd --reload
