0d4d7d949a59d03f8f18b8e263dbc2b4c37f2007
Sadaf Syed <sadsyed@localhost.localdomain>
2022-01-06 17:45:57 -0500

    updated with github ssh key instructions

--- setup_openSUSE.sh
+++ setup_openSUSE.sh
@@ -2,6 +2,13 @@
 
 ## install git
 sudo zypper install --no-confirm git-core
+# to setup github with ssh key
+# ssh-keygen -t ed25519 -C "<email>"
+# cat ~/.ssh/id_ed25519.pub
+# log into github account -> settings -> add SSH key and paste the contents from above cmd
+# set your remote URL to a form that supports SSH
+# git remote set-url origin git+ssh://git@github.com:/<username>/<reponame>.git
+# git remote show origin
 
 ## install openssh
 sudo zypper refresh
