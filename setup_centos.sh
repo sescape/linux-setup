6af20a69845a55f6ec68d0ab1267dc22fc9d3705
sadsyed <sadsyed@localhost.localdomain>
2022-01-09 18:24:36 -0500

    added docker install from repo but it is failing

--- setup_centos.sh
+++ setup_centos.sh
@@ -27,3 +27,30 @@ sudo systemctl enable sshd
 
 # open ssh config
 # sudo vim /etc/ssh/sshd_config
+
+# ----- Installing latest docker engine -----
+echo -e "\n ----- Installing yum-utils-----"
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+#sudo yum-config-manager --enable docker-ce-nightly
+#sudo yum-config-manager --enable docker-ce-test
+#sudo yum-config-manager --disable docker-ce-nightly
+echo -e "\n ----- Installing docker managed by systemctl -----"
+sudo yum install docker-ce docker-ce-cli containerd.io
+sudo docker --version
+
+# ----- installing docker managed by systemctl -----
+# echo -e "\n ----- Installing docker managed by systemctl -----"
+# sudo yum update -y
+# sudo yum -y install docker-io
+# sudo yum -y install docker
+# sudo systemctl start docker
+
+# ----- installing docker ----
+#echo -e "\n ----- Installing docker -----"
+# sudo yum update -y
+# sudo curl -fsSL https://get.docker.com -o get-docker.sh
+# sudo sh get-docker.sh
+# sudo docker --version
+
+
