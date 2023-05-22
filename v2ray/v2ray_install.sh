83ab2066ff4a0551f38464711efcc05c1b77f4ff
Team Ummi <team.ummi@gmail.com>
2023-05-22 11:59:15 -0500

    server config and uuid install

--- v2ray/v2ray_install.sh
+++ v2ray/v2ray_install.sh
@@ -18,9 +18,20 @@ sudo apt-get full-upgrade
 # install curl
 sudo apt-get install curl -y
 
+# date
+date -R
+
+# install and generate uuid
+sudo apt-get install uuid-runtime
+uuidgen
+
 # get v2ray script
 curl -O https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh
 
 # execute v2ray installation script
 sudo bash install-release.sh
 v2ray version
+
+# edit v2ray.service to add VMESS env and reload and restart v2ray
+systemctl daemon-reload
+systemctl restart v2ray
